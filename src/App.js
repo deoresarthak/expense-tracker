6d7112cbdc97ae5a80b7333d05e5a6cd2f713c3d
Sarthak Deore <sarthakdeore2@gmail.com>
2022-06-07 21:36:39 +0530

    module 4 complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -23,9 +24,13 @@ function App() {
       date: new Date(2022, 4, 18),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  };
   return (
     <div>
-      <h2>Let's get going!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpensData) => {
+    const expenseData = { ...enteredExpensData, id: Math.random().toString() };
+
+    props.onAddExpense(expenseData);
+  };
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />;
+    </div>
+  );
+};
+
+export default NewExpense;
